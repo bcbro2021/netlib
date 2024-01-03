649e56ebc7b769e722fb8d42a88e646a0bdd4c41
bcbro2021 <74782934+bcbro2021@users.noreply.github.com>
2024-01-03 23:33:03 +0530

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+pub mod socket {
+    use std::io::{Read, Write};
+    use std::net::{TcpListener, TcpStream};
+
+    // Client
+    pub struct Client {
+        pub addr: String,
+        pub cli: TcpStream
+    }
+
+    pub fn create_client(addr: &str) -> Client {
+        return Client {addr: addr.to_string(),cli: TcpStream::connect(addr).unwrap()};
+    }
+
+    pub fn close_client(mut sock: &TcpStream) {
+        sock.flush().unwrap();
+    }
+
+    // Server
+    pub struct Server {
+        pub addr: String,
+        pub ser: TcpListener
+    }
+
+    pub fn create_server(addr: &str) -> Server {
+        return Server {addr: addr.to_string(),ser: TcpListener::bind(addr.to_string()).unwrap()};
+    }
+
+    // server and client funcs
+    pub fn send(mut sock: &TcpStream, msg: &str) {
+        sock.write_all(msg.as_bytes()).unwrap();
+    }
+
+    pub fn recv(mut sock: &TcpStream, buf: usize) -> String {
+        let mut buffer = vec![0; buf];
+        let size = sock.read(&mut buffer).unwrap();
+        return String::from_utf8_lossy(&buffer[..size]).to_string();
+    }
+}
